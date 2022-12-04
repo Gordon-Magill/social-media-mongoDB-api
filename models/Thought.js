f34419e4065b89f08a3085c9c4702bf0740ef23b
Gordon-Magill <gordon.magill@gmail.com>
2022-12-03 18:28:10 -0800

    Improved Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,33 +1,53 @@
 // Include schema for Reactions as a subdocument to Thought
-const { Schema, model } = require("mongoose");
+const { Schema, model, models } = require("mongoose");
 
 const thoughtSchema = new Schema(
-    {
-        thoughtText:{
-            type: String,
-            required:true,
-            minLength: 1,
-            maxLength: 280,
-        },
-        createdAt:{
-            type: Date,
-            default: Date.now
-        },
-        username:{
-            type:String,
-            required: true,
-        },
-        reactions:[{
-            reactionID: {
-                type: Schema.Types.ObjectId,
-                default: new Schema.Types.ObjectId
-            },
-            reactionBody:{},
-            username:{},
-            createdAt:{}
-        }],
+  {
+    thoughtText: {
+      type: String,
+      required: [true, "Thought text is required!"],
+      minLength: 1,
+      maxLength: 280,
     },
-    {
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: date => date.toDateString()
+    },
+    username: {
+      type: String,
+      required: [true, "Username for thought is required!"],
+    },
+    reactions: [
+      {
+        reactionID: {
+          type: Schema.Types.ObjectId,
+          default: new Schema.Types.ObjectId(),
+        },
+        reactionBody: {
+          type: String,
+          require: true,
+          maxLength: 280,
+        },
+        username: {
+          type: String,
+          required: true,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+          get: date => date.toDateString()
+        },
+      },
+    ],
+  },
+  {}
+);
 
-    }
-)
+thoughtSchema.virtuals("reactionCount").get(function () {
+  return reactions.length;
+});
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = { Thought };
